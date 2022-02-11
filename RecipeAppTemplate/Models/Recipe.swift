a2c38507ad441fc1a96c2d3d045a555190651eb1
Dietrich Bragg <dietrich.bragg@gmail.com>
2022-02-11 15:42:20 -0500

    Initial Commit

---
+++ RecipeAppTemplate/Models/Recipe.swift
@@ -0,0 +1,35 @@
+//
+//  Recipe.swift
+//  RecipeAppTemplate
+//
+//  Created by Dietrich Bragg on 12/20/21.
+//
+
+
+import Foundation
+
+class Recipe: Identifiable, Decodable {
+    
+    var id:UUID?
+    var name:String
+    var featured:Bool
+    var image:String
+    var description:String
+    var prepTime:String
+    var cookTime:String
+    var totalTime:String
+    var servings:Int
+    var highlights:[String]
+    var ingredients:[Ingredient]
+    var directions:[String]
+    
+}
+
+class Ingredient: Identifiable, Decodable {
+    
+    var id:UUID?
+    var name:String
+    var num:Int?
+    var denom:Int?
+    var unit:String?
+}
---
+++ RecipeAppTemplate/RecipeAppTemplate.swift
@@ -0,0 +1,16 @@
+//
+//  RecipeAppTemplateApp.swift
+//  Shared
+//
+//  Created by Dietrich Bragg on 12/20/21.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppTemplate: App {
+    var body: some Scene {
+            WindowGroup {
+                RecipeTabView()        }
+    }
+}
